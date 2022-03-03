1978c73b66c9f45df8c1f00ee07791d463e78186
Tim Steffens <tim.steffens@greenbone.net>
2022-03-03 10:08:22 +0100

    polishing

--- clean.sh
+++ clean.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # example crontab 15 19 * * * /Users/tim/scripts/clean.sh dir /Users/tim/Downloads
+# see also https://osxdaily.com/2020/04/27/fix-cron-permissions-macos-full-disk-access/
 
 script=$0
 type=$1
@@ -9,7 +10,7 @@ dir=$2
 if [[ "$type" == "file" ]]; then
     file=$3
     mv "$file" ~/.Trash
-    echo "$file" >> "$2"/cleanedFiles
+    echo "$file" >> "$dir"/cleanedFiles
 fi
 
 if [[ "$type" == "dir" ]]; then
